918b810e62c53398fc683d2835659d28756b2c83
MyUsername <emanuele.marini37@gmail.com>
2021-06-15 15:38:36 +0200

    prima stesura

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*PARTE 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. Stampare a schermo 
+attraverso un ciclo for-in tutte le proprietà dell’oggetto.
+*/
+
+var student = {
+      stuName : 'Gianni',
+      stuSurname : 'Benni',
+      stuAge : 20,
+}
+
+console.log(student)
+
+
+
+
+/*PARTE 2
+Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.  
+Ricordatevi, è un array, quindi? for.
+*/
+
+
+
+
+
+
+
+
+
+
+/*PARTE 3
+Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà -
+ di aggiungere UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
+*/
+
+
